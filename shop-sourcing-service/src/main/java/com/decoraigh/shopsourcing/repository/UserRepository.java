643b0e9d09fd3e54d7af42de58d838bca13750a1
Elizabeth Esseba Ansah <elizabethesseba25@gmail.com>
2026-06-27 04:10:54 +0000

    Build shop-sourcing-service with shop entity, repository, service and controller

--- shop-sourcing-service/src/main/java/com/decoraigh/shopsourcing/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.decoraigh.shopsourcing.repository;
-
-import com.decoraigh.shopsourcing.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
